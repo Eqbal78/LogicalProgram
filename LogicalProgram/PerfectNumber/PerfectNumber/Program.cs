2bc8188ee651da34f1fb97f7f4d3f2c28f1c414d
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-04 12:54:20 +0530

    Find the perfect number

---
+++ LogicalProgram/PerfectNumber/PerfectNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PerfectNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, sum;
+
+            Console.Write("Enter the  number : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            sum = 0;
+
+            Console.Write("The positive divisor  : ");
+            for (i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    sum = sum + i;
+                    Console.Write("{0} " ,i);
+                }
+                
+            }
+            Console.Write("\nThe sum of the divisor is : {0}", sum);
+            if (sum == n)
+                Console.Write("\nSo, the number is perfect.");
+            else
+                Console.Write("\nSo, the number is not perfect.");
+            Console.Write("\n");
+        }
+    }
+}
