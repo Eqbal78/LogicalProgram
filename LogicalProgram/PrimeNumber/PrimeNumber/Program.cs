a8a2310bfd332336c09a7f16b1ebf1ab962d8f76
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-04 13:44:13 +0530

    Find the prime number

---
+++ LogicalProgram/PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n , a = 0;
+            Console.WriteLine("Enter the Number: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                 if (n % i == 0)
+                 {
+                     a++;
+                 }
+            }
+            if (a == 2)
+            {
+                Console.WriteLine("{0} is a Prime Number", n);
+            }
+            else
+            {
+                Console.WriteLine("Not a Prime Number");
+            }
+        }
+    }
+}
