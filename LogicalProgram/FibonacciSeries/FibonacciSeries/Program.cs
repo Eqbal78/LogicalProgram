353b9fe83fe374447a628cb3a7e1b10b9c692551
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-04 12:37:01 +0530

    Find the Fibonacci Series

--- LogicalProgram/FibonacciSeries/FibonacciSeries/Program.cs
+++ LogicalProgram/FibonacciSeries/FibonacciSeries/Program.cs
@@ -14,7 +14,7 @@ namespace FibonacciSeries
             number = int.Parse(Console.ReadLine());
 
             Console.Write(n1 + " " + n2 + " ");    
-            for (i = 2; i < number; ++i) 
+            for (i = 2; i < number; i++) 
             {
                 n3 = n1 + n2;
                 Console.Write(n3 + " ");
