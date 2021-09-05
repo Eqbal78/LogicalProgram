900432d111184d633060b8d81a31b5ec87437e06
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-05 21:47:05 +0530

    Find the Monthly Payment Amount

---
+++ LogicalProgram/monthlyPayment/monthlyPayment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace monthlyPayment
+{
+    class Program
+    {
+        static double Amount(int p, double r, int y)
+        {
+            y = y * 12;
+            r = r / (100 * 12);
+            return  (p * r) / (1 - Math.Pow(1 + r, -y));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("principal");
+            int p = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Rate");
+            double r = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Year");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Payment Amount: " + Amount(p, r, y));        
+        }
+    }
+}
