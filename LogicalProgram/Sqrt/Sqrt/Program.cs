389ec0cbbe1647fc006ecb480f6578e6aa1ced69
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-05 21:49:19 +0530

    Square Root of a number

---
+++ LogicalProgram/Sqrt/Sqrt/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Sqrt
+{
+    class Program
+    {
+        static double squareRoot(double n, double l)
+        {
+            
+            double x = n;
+            double root;
+
+            int count = 0;
+            while (true)
+            {
+                count++;
+                root = 0.5 * (x + (n / x));
+
+                if (Math.Abs(root - x) < l)
+                    break;
+                x = root;
+            }
+
+            return x;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Input");
+            double n = double.Parse(Console.ReadLine());
+            double l = 0.000000000000001;
+
+            Console.WriteLine(squareRoot(n, l));
+        }
+    }
+}
