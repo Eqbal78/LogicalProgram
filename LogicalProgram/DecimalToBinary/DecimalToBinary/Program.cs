7df478fd71fa467e05d14e6ca575d2e55b7ffb62
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-05 23:19:51 +0530

    Convert Decimal to Binary Number

---
+++ LogicalProgram/DecimalToBinary/DecimalToBinary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DecimalToBinary
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int n, i;
+            int[] a = new int[10];
+            Console.Write("Enter the number to convert: ");
+            int decimalNumber = int.Parse(Console.ReadLine());
+
+            int remainder;
+            string result = string.Empty;
+            while (decimalNumber > 0)
+            {
+                remainder = decimalNumber % 2;
+                decimalNumber /= 2;
+                result = remainder.ToString() + result;
+            }
+            Console.Write("Binary of the given number " + result );
+          
+        }
+    }
+}
