555fdeda2d1337a736aef1722ace0afbcf2cfa59
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-05 21:31:43 +0530

    Vending Machine Programming Code

---
+++ LogicalProgram/VendingMachine/VendingMachine/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace VendingMachine
+{
+    class Program
+    {
+        public static void countCurrency(int amount)
+        {
+            int[] notes = new int[] { 2000, 500, 200, 100, 50, 20, 10, 5, 1 };
+            int[] noteCounter = new int[9];
+
+            
+            for (int i = 0; i < 9; i++)
+            {
+                if (amount >= notes[i])
+                {
+                    noteCounter[i] = amount / notes[i];
+                    amount = amount - noteCounter[i] * notes[i];
+                    Console.WriteLine("amount"+amount);
+                }
+            }
+
+            
+            Console.WriteLine("Currency Count ->");
+            for (int i = 0; i < 9; i++)
+            {
+                if (noteCounter[i] != 0)
+                {
+                    Console.WriteLine(notes[i] + " : " + noteCounter[i]);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Amount:");
+            int amount = int.Parse(Console.ReadLine());
+            countCurrency(amount);
+        }
+    }
+}
